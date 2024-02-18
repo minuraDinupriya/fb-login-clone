8762430c64a60e4e465beec5bae6671ccc70e220
Minura Dinupriya <mranaweera793@gmail.com>
2024-02-18 11:08:28 +0530

    sketch done

--- fb-login-clone/src/App.jsx
+++ fb-login-clone/src/App.jsx
@@ -1,27 +1,63 @@
 import "./App.css";
 
 function App() {
-  return <>
-  <>
+  return (
+    <>
       <div className="container">
         <div className="row"></div>
         <div className="row">
-          <div className="card" >
+          <div className="card">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
+              <div className="row">
+                <h5 className="card-title">Log in to Facebook</h5>
+              </div>
+
+              <div className="row">
+                <input
+                  type="text"
+                  className="form-control mt-3"
+                  placeholder="Email address or phone number"
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                ></input>
+              </div>
+
+              <div className="row mt-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Password"
+                  aria-label="Recipient's username"
+                  aria-describedby="button-addon2"
+                ></input>
+              </div>
+
+              <div className="row"></div>
+
+              <div className="row mt-3 ">
+                <a href="#" className="btn btn-primary">
+                  Log in
+                </a>
+              </div>
+
+              <div className="row"></div>
+
+              <div className="row mt-2 ">
+                <a href="#" className="ms-5 btn btn-success w-75 justify-content-center d-flex">
+                  Create new account
+                </a>
+              </div>
+
               <p className="card-text">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
             </div>
           </div>
         </div>
       </div>
     </>
-  </>;
+  );
 }
 
 export default App;
