ad9ed82860e4e716622e0d3f6b3334dc9cc39d6a
Minura Dinupriya <mranaweera793@gmail.com>
2024-02-18 12:15:39 +0530

    finalized

--- fb-login-clone/src/App.jsx
+++ fb-login-clone/src/App.jsx
@@ -4,11 +4,18 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="row h1 fw-bold mb-3 d-flex justify-content-center text-center" style={{color:"#0398fc"}}>facebook</div>
+        <div
+          className="row h1 fw-bold mb-3 d-flex justify-content-center text-center"
+          style={{ color: "#0398fc" }}
+        >
+          facebook
+        </div>
         <div className="row">
-          <div className="card custom-card" style={{width:'450px',height:'auto'}}>
+          <div
+            className="card custom-card"
+            style={{ width: "450px", height: "auto" }}
+          >
             <div className="card-body">
-              
               <div className="row">
                 <h5 className="card-title">Log in to Facebook</h5>
               </div>
@@ -25,7 +32,7 @@ function App() {
 
               <div className="row mt-3">
                 <input
-                  type="text"
+                  type="password"
                   className="form-control"
                   placeholder="Password"
                   aria-label="Recipient's username"
@@ -33,18 +40,18 @@ function App() {
                 ></input>
               </div>
 
-              
-
               <div className="row mt-3 ">
                 <a href="#" className="btn btn-primary">
                   Log in
                 </a>
               </div>
 
-              <div className="row link-info text-center d-flex justify-content-center mb-2 mt-2">Forgotten acount?</div>
+              <div className="row  text-center d-flex justify-content-center mb-2 mt-2 link-opacity-25" >
+                <a href="#">Forgotten acount?</a>
+              </div>
 
               <div className="row">
-              <div class="separator">or</div>
+                <div className="separator">or</div>
               </div>
 
               <div className="row mt-2 d-flex justify-content-center">
@@ -52,7 +59,6 @@ function App() {
                   Create new account
                 </a>
               </div>
-
             </div>
           </div>
         </div>
